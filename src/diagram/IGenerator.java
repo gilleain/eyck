dcb28555efed58d7b91e3cdcc73d61b8cfd7e1b9
maclean <gilleain.torrance@gmail.com>
2010-10-07 18:08:57 +0100

    Pass the information necessary for transform to generator - bit nasty

--- src/diagram/IGenerator.java
+++ src/diagram/IGenerator.java
@@ -1,9 +1,11 @@
 package diagram;
 
+import javax.vecmath.Point2d;
+
 import diagram.element.IDiagramElement;
 
 public interface IGenerator<T> {
     
-    public IDiagramElement generate(T model);
+    public IDiagramElement generate(T model, Point2d dc, double scale, Point2d cc);
 
 }
